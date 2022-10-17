5982a2653d4627dc9fe050a717adeaf8726b3227
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2022-10-17 19:48:36 -0400

    work in progress `/get-room` command

---
+++ commands/get-room.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('get-room')
+		.setDescription('rec room')
+		.addSubcommand(subcommand =>
+			subcommand
+			.setName("id")
+			.setDescription("Get a Rec Room from its ID")
+			.addStringOption(string =>
+				string.setName("id")
+					.setRequired(true)
+					.setDescription("the ID of the room")))
+		.addSubcommand(subcommand =>
+			subcommand
+			.setName("name")
+			.setDescription("Get a Rec Room from its name")
+			.addStringOption(string =>
+				string.setName("name")
+					.setRequired(true)
+					.setDescription("The name of the room"))),
+	async execute(interaction) {
+		const cmd = interaction.options.getSubcommand()
+		await interaction.deferReply()
+
+		let id
+
+		if(cmd == "id"){
+			id = `/${interaction.options.getString("id")}`
+		} else if(cmd == "name"){
+			id = `?name=${interaction.options.getString("name")}`
+		}
+
+		try {
+			const response = await fetch(`https://rooms.rec.net/rooms${id}`)
+			const json = await response.json();
+
+			if (json.title) {
+				interaction.editReply("❌ **Invalid room name/id!**"); 
+				return;
+			}
+
+			//https://rooms.rec.net/rooms/1
+
+			const embed = new EmbedBuilder()
+				.setTitle(`^${json.Name} - ${json.RoomId}`)
+				.setURL(`https://rec.net/room/${json.Name}`)
+				.setImage(`https://img.rec.net/${json.ImageName}`)
+				.setDescription(json.Description)
+				.addFields(
+					{ name: 'Room ID (deprecated)', value: `${json.RoomId}`, inline: true },
+					{ name: 'Doom Room?', value: `${json.IsDorm}`, inline: true },
+					{ name: 'Max Players', value: `${json.MaxPlayers}`, inline: true },
+					{ name: 'Creator (ID)', value: `${json.CreatorAccountId}`, inline: true },
+				)
+			if(cmd == "id"){interaction.editReply({ content:"⚠️ **Room IDs are no longer supported by the Rec Room API.**", embeds: [embed] });} else {interaction.editReply({ embeds: [embed] });}
+		} catch(e) {
+			console.log(e)
+			interaction.editReply("❌ **There was an error with your request.\n\nEither the requested room is invalid, or there's an internal issue.**"); 
+			return;
+		}
+	},
+};
