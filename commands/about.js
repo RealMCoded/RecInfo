9daac39444f4bef6d78bc350efe47e64ed704871
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-02-09 19:37:37 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -6,15 +6,13 @@ module.exports = {
 		.setName('about')
 		.setDescription('About the bot!'),
 	async execute(interaction) {
-		const cmd = interaction.options.getSubcommand()
-
         var revision = require('child_process')
                     .execSync('git rev-parse HEAD')
                     .toString().substring(0, 7);
 
 		const embed = new EmbedBuilder()
 			.setTitle(`About RecInfo`)
-			.setDescription(`Version ${project.version} ([commit \`${revision}\`](https://github.com/RealMCoded/RecInfo/commit/${revision}))\n\nBot created by [stuartt#5679](https://discord.com/users/284804878604435476) // [@stuartt](https://rec.net/user/stuartt)`)
-		interaction.editReply({ embeds: [embed] });
+			.setDescription(`Version ${project.version} ([commit \`${revision}\`](https://github.com/RealMCoded/RecInfo/commit/${revision}))\n\nBot created by [stuartt#5679](https://discord.com/users/284804878604435476 "My Discord Profile") // [@stuartt](https://rec.net/user/stuartt "My rec.net")`)
+		interaction.reply({ embeds: [embed] });
 	},
 };
