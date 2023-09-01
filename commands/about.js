87127061727298e68b6fb6a0f7d1403012bea670
stuartt_mcoded <34777184+RealMCoded@users.noreply.github.com>
2023-08-31 20:32:35 -0400

    0.1.5

--- commands/about.js
+++ commands/about.js
@@ -14,7 +14,7 @@ module.exports = {
 		const embed = new EmbedBuilder()
 			.setTitle(`About RecInfo`)
 			.setColor(randomColor())
-			.setDescription(`Version ${project.version} ([commit \`${revision}\`](https://github.com/RealMCoded/RecInfo/commit/${revision}))\n\nBot created by [stuartt#5679](https://discord.com/users/284804878604435476 "My Discord Profile") // [@stuartt](https://rec.net/user/stuartt "My rec.net")`)
+			.setDescription(`Version ${project.version} ([commit \`${revision}\`](https://github.com/RealMCoded/RecInfo/commit/${revision}))\n\nBot created by [stuartt](https://discord.com/users/284804878604435476 "My Discord Profile") // [@stuartt (RecNet)](https://rec.net/user/stuartt "My rec.net")`)
 		interaction.reply({ embeds: [embed] });
 	},
 };
